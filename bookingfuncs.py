3f40bda17541c9f2c145f46f2d6cfb60616f861a
yacoola <95629278+yacoola@users.noreply.github.com>
2024-08-18 16:57:36 -0400

    Update bookingfuncs.py

--- bookingfuncs.py
+++ bookingfuncs.py
@@ -115,6 +115,8 @@ def get_valid_session(communauto_cred):
 
         page.goto(LOGIN_URL)
         page.wait_for_load_state('networkidle')
+        page.screenshot(path="screenshot.png")
+        print(page.content())
         page.locator('input[name="Username"]').fill(USER)
         page.locator('input[name="Password"]').fill(PASS)
         page.click('button.MuiButton-root.MuiButton-contained.MuiButton-containedPrimary')
