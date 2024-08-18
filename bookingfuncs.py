a368dedf12516390c743df216dc82f4c7a25904a
yacoola <95629278+yacoola@users.noreply.github.com>
2024-08-18 16:35:48 -0400

    Update bookingfuncs.py

--- bookingfuncs.py
+++ bookingfuncs.py
@@ -114,7 +114,7 @@ def get_valid_session(communauto_cred):
         page = context.new_page()
 
         page.goto(LOGIN_URL)
-        page.wait_for_load_state() # the promise resolves after "load" event.
+        page.wait_for_selector('input[name="Username"]')
         page.locator('input[name="Username"]').fill(USER)
         page.locator('input[name="Password"]').fill(PASS)
         page.click('button.MuiButton-root.MuiButton-contained.MuiButton-containedPrimary')
